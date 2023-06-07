41ebbab875e1b558f630a75d6e91c94c9eb1b549
Ruxin Qu <rosheenqu@gmail.com>
2023-06-06 17:45:50 -0700

    generate cookie after user login or signup

--- routes/user.js
+++ routes/user.js
@@ -8,11 +8,11 @@ router.post("/register", async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) {
       const newUser = await User.create({ email, password });
-      newUser
-        ? res.status(200).json(newUser)
-        : res.status(404).json({ message: "Failed to find the user" });
+      !newUser
+        ? res.status(404).send({ message: "Failed to find the user" })
+        : res.status(200).send({ token: signToken(newUser), user: newUser });
     } else {
-      res.status(400).json({ message: "This email already exist" });
+      res.status(400).send({ message: "This email already exist" });
     }
   } catch (err) {
     console.log(err);
@@ -24,12 +24,12 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      res.status(404).json({ message: "No user with this email found" });
+      res.status(404).send({ message: "No user with this email found" });
       return;
     }
     const checkPw = await user.isCorrectPassword(password);
     if (!checkPw) {
-      res.status(404).json({ message: "Incorrect password" });
+      res.status(404).send({ message: "Incorrect password" });
       return;
     }
     const token = signToken(user);
