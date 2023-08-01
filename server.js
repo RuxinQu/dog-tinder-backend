e4d1a4709fb9ad79662db7e93bce0b88eb25bc69
Ruxin Qu <rosheenqu@gmail.com>
2023-07-31 22:43:52 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
 db.once("open", () => {
   app.listen(port, () => {
     console.log(`server is listening to port ${port}!`);
